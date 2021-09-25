cc6e2a651a227c0cca8c58545e81edf968045565
Apochen <15309036477@qq.com>
2021-09-25 15:21:56 +0800

    [sort] add quick sort.

--- src/sort/quick_sort.rs
+++ src/sort/quick_sort.rs
@@ -1,3 +1,28 @@
+use rand::{Rng, thread_rng};
+
 pub fn sort(nums: &mut Vec<i32>, l: usize, r: usize) {
-    
+    if l + 1 < r {
+        let m = partition(nums, l, r);
+        sort(nums, l, m);
+        sort(nums, m + 1, r);
+        // println!("{:?} with partition {}", nums, m);
+    }
+}
+
+fn partition(nums: &mut Vec<i32>, l: usize, r: usize) -> usize {
+
+    // Get a random number
+    let mut rng = thread_rng();
+    let random_pos = rng.gen_range(l..r);
+
+    nums.swap(r-1, random_pos);
+    let mut low = l;
+    for i in l..r-1 {
+        if nums[i] < nums[r - 1] {
+            nums.swap(i, low);
+            low += 1;
+        }
+    }
+    nums.swap(low, r - 1);
+    low
 }
